420c80a5793465b74adb1f0fd56fa7fac79715c5
LucasGuiron <lucasguirondj@hotmail.com>
2024-06-29 13:41:23 -0300

    testes

--- src/javascript/serve.js
+++ src/javascript/serve.js
@@ -5,13 +5,16 @@ const port = 3000;
 
 const YOUTUBE_API_KEY = 'No comentario ao lado'; // chave de autorização = AIzaSyDRCvpg52qAjiiUrf8Td6NczMw0pZCFazQ 
 
-app.get('/search', async (req, res) => {
-    const query = req.query.q;
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${query}&key=${YOUTUBE_API_KEY}`);
-    const data = await response.json();
-    res.json(data);
+app.get('/', async (req, res) => {
+    //const query = req.query.q;
+    //const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${query}&key=${YOUTUBE_API_KEY}`);
+    //const data = await response.json();
+    //res.json(data);
+    const users = ['lucas', 'yasmim', 'janaina'];
+
+    return res.json({users});
 });
 
-app.listen(port, () => {
-    console.log(`BFF listening at http://localhost:${port}`);
-});
+//app.listen(port, () => {
+//    console.log(`BFF listening at http://localhost:${port}`);
+//});
