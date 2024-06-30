a46818e050da065c83f16de240160a20b239e704
LucasGuiron <lucasguirondj@hotmail.com>
2024-06-30 16:29:07 -0300

    tentativa de consumo de api falhou

--- src/javascript/serve.js
+++ src/javascript/serve.js
@@ -1,20 +1,36 @@
-import express from 'express';
-import fetch from 'node-fetch';
-const app = express();
-const port = 3000;
+ // chave de autorização = AIzaSyDRCvpg52qAjiiUrf8Td6NczMw0pZCFazQ 
 
-//const YOUTUBE_API_KEY = 'AIzaSyDRCvpg52qAjiiUrf8Td6NczMw0pZCFazQ'; // chave de autorização = AIzaSyDRCvpg52qAjiiUrf8Td6NczMw0pZCFazQ 
-
-app.get('/', async (req, res) => {
-    //const query = req.query.q;
-    //const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${query}&key=${YOUTUBE_API_KEY}`);
-    //const data = await response.json();
-    //res.json(data);
-    const users = ['lucas', 'yasmim', 'janaina'];
-
-    return res.json({users});
-});
-
-app.listen(port, () => {
-    console.log(`BFF listening at http://localhost:${port}`);
-});
+ const express = require('express');
+ const fetch = require('node-fetch');
+ const app = express();
+ const port = 3000;
+ 
+ const apiKey = 'AIzaSyDRCvpg52qAjiiUrf8Td6NczMw0pZCFazQ';
+ 
+ app.get('/youtube-search', async (req, res) => {
+     const query = req.query.q || 'cat videos';
+     const maxResults = req.query.maxResults || 10;
+     const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=${maxResults}&q=${query}&key=${apiKey}`;
+     
+     const requestOptions = {
+         method: "GET",
+         redirect: "follow"
+     };
+ 
+     try {
+         const response = await fetch(url, requestOptions);
+         if (!response.ok) {
+             throw new Error(`Erro na resposta da API: ${response.statusText}`);
+         }
+         const data = await response.json();
+         res.json(data);
+     } catch (error) {
+         console.error('Erro ao buscar vídeos do YouTube:', error);
+         res.status(500).json({ error: 'Erro ao buscar vídeos do YouTube' });
+     }
+ });
+ 
+ app.listen(port, () => {
+     console.log(`Servidor rodando em http://localhost:${port}`);
+ });
+ 
